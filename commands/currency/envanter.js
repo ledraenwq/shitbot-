8bbfefc479d1155e3072e23f7b3e4bd39ff49396
unknown <cinarozlu@gmail.com>
2020-09-11 15:37:51 +0300

    Envanter

--- commands/currency/envanter.js
+++ commands/currency/envanter.js
@@ -10,17 +10,21 @@ function wait(ms) {
     } while (d2 - d < ms);
 }
 
-exports.run = async (bot, message, args) => {
-    let items = db.fetch(message.author.id)
-    let user = message.author || message.mentions.users.first()
-    if (items === null) items = "Bu kullanıcı fakir (hiç bir şeyi yok abo)"
+exports.run = async (client, message, args) => {
+    try {
+        let items = db.fetch(message.author.id)
+        let user = message.author || message.mentions.users.first()
+        if (items === null) items = "Bu kullanıcı fakir (hiç bir şeyi yok abo)"
 
-    let embed = new Discord.MessageCollector()
-        .setTitle(`${message.author.username}'in envanteri`)
-        .setColor("RANDOM")
-        .addField("Eşyalar", items)
-        .setTimestamp()
-    message.channel.send(embed)
+        let embed = new Discord.MessageEmbed()
+            .setTitle(`${message.author.username}'in envanteri`)
+            .setColor("RANDOM")
+            .addField("Eşyalar", items)
+            .setTimestamp()
+        message.channel.send(embed)
+    } catch (e) {
+        message.channel.send(e.message)
+    }
 }
 
 exports.help = {
